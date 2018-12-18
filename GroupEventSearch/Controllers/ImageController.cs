62abee3c04744bd94146a8d8b553235a682e5bf9
b-lor <blorwi@gmail.com>
2018-12-18 11:49:56 -0600

    combined uploading image, restaurant api

---
+++ GroupEventSearch/Controllers/ImageController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using GroupEventSearch.Models;
+using System.Diagnostics;
+
+namespace GroupEventSearch.Controllers
+{
+    public class ImageController : Controller
+    {
+        private readonly ImageController he;
+
+        public string WebRootPath { get; private set; }
+
+        public ImageController(ImageController e)
+        {
+            he = e;
+        }
+
+        public IActionResult ShowFields(string fullName, IFormFile pic)
+        {
+            ViewData["fname"] = fullName;
+            if (pic != null)
+            {
+                var fileName = Path.Combine(he.WebRootPath, Path.GetFileName(pic.FileName));
+                pic.CopyTo(new FileStream(fileName, FileMode.Create));
+                ViewData["fileLocation"] = "/" + Path.GetFileName(pic.FileName);
+            }
+            return View();
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ GroupEventSearch/Models/GeocodeGoogle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupEventSearch.Models
+{
+    public class GeocodeGoogle
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
